4d124e2e6d63a5d7223b0b6d8468b2594ab86547
ahmed-kawsar <kawsar4698@gmail.com>
2022-12-04 14:51:30 +0600

    fixed minor css issue

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -5,35 +5,31 @@ const ProductScreen = ({ products }) => {
   const { slug } = useParams()
   const product = products.find((item) => item.slug === slug)
   return (
-    <main className='page p-4'>
+    <main className='page container'>
       <Link to='/'>
         <div className='flex items-center gap-2 my-4'>
           <GrReturn /> Back To Home
         </div>
       </Link>
 
-      <div className='grid md:grid-cols-5'>
-        <div className='col-span-3'>
+      <div className='grid md:grid-cols-5 p-4'>
+        <div className='col-span-3 ml-5'>
           <img src={product.image} alt={product.name} className='w-full' />
         </div>
-        <div className='col-span-1 pl-4'>
+        <div className='col-span-2 pl-4 ml-6 flex flex-col gap-3'>
           <h2 className='text-2xl'>{product.name}</h2>
           <p>Category: {product.category}</p>
           <p>
             Rating: {product.rating} of {product.numReviews}
           </p>
           <p>Description: {product.description}</p>
-        </div>
-        <div className='border h-1/5 p-2 shadow-md grid'>
-          <div className='flex justify-between'>
-            <p>Price</p>
-            <p>{product.price}</p>
+          <div>
+            <p>
+              Price: <strong>${product.price}</strong>
+            </p>
+            <p>Status{product.countInStock < 1 ? 'Out Of Stock' : 'InStock'}</p>
           </div>
-          <div className='flex justify-between'>
-            <p>Status</p>
-            <p>{product.countInStock < 1 ? 'Out Of Stock' : 'InStock'}</p>
-          </div>
-          <button className='primary-btn'>Add to Cart</button>
+          <button className='primary-btn w-32'>Add to Cart</button>
         </div>
       </div>
     </main>
